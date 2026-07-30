f8d404cc191dfc3b2f24c39778ddf51644334bdd
Tanisha borana <101504198+Tanisha-b3@users.noreply.github.com>
2026-07-30 21:40:33 +0530

    Added

--- Client/src/pages/public/sections/Testimonials.tsx
+++ Client/src/pages/public/sections/Testimonials.tsx
@@ -2,6 +2,7 @@ import { useRef, useState, useEffect } from "react";
 import { motion, useInView, useReducedMotion, AnimatePresence } from "framer-motion";
 import { testimonials } from "./data";
 import { GradientText } from "./anim";
+import { injectSchema, REVIEW_SCHEMA } from "../../../utils/schema";
 
 interface TestimonialItem {
   name: string;
